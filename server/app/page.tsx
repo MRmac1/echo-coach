1fd688fd818783ed97695c63fc6936853c867b9b
zhoujicheng <zhoujicheng@sensetime.com>
2025-05-23 11:00:25 +0800

    feat: 0523

--- server/app/page.tsx
+++ server/app/page.tsx
@@ -2,8 +2,12 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col items-center'>
       <section className='mx-auto flex'>Home with Tabs</section>
+      <section className='mx-auto flex'>
+        <section>hhahah</section>
+      </section>
+      <button className='bg-sky-500 hover:bg-sky-100'>...Save changes</button>
       <footer>
         <Link href='/record'>to Record</Link>
       </footer>
